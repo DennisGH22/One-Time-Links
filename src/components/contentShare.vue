5fe385d5b9499593761c13eff1ab108a9b520b09
Tebogo Ndaba <110781768+DennisGH22@users.noreply.github.com>
2022-12-24 19:25:15 +0200

    Add files via upload

---
+++ src/components/contentShare.vue
@@ -0,0 +1,100 @@
+<script lang="ts">
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
+import Editor from "@tinymce/tinymce-vue";
+import DOMPurify from 'dompurify';
+import dataLink from "../components/dataLink.vue";
+import { processExpression } from "@vue/compiler-core";
+
+export default {
+  name: "content",
+  data() {
+    return {
+      secretData: "",
+      uniqueLink: "",
+      generateURL: "",
+      awaiting: "0",
+      disabled: "1",
+      visibility: true,
+    }
+  },
+  components: {
+    editor: Editor,
+    dataLink,
+  },
+  methods: {
+    async generateLink() {
+      if (this.secretData === '') return;
+      try {
+        this.awaiting = "1";
+        this.uniqueLink = (uuidv4());
+        const post = await axios.post("http://localhost:3000/share", {
+          data: DOMPurify().sanitize(this.secretData, { USE_PROFILES: { html: false } }),
+          data_permalink: this.uniqueLink
+        });
+        const status = post.status;
+        this.generateURL = this.uniqueLink;
+        if (this.generateURL !== '' && status === 200) {
+          const protocol = location.protocol;
+          const domain = location.hostname;
+          const port = ':5173';
+          domain === 'localhost' ? port : '';
+          this.generateURL = `${protocol}//${domain}${port}/${this.generateURL}`;
+          this.awaiting = "0"
+          this.disabled = "0";
+        }
+      } catch(error) {
+        throw error;
+      }
+    },
+    copyURL(visibility: boolean) {
+      navigator.clipboard.writeText(this.generateURL); 
+      this.visibility = visibility;
+      setTimeout(() => {
+        this.visibility = true;
+      }, 3000);
+    },
+  }
+}
+</script>
+
+<template>
+  <div class="wrapper">
+    <div class="steps">
+      <h1>How to use it...</h1>
+      <div class="step">
+        <div class="num">
+          <h2>1</h2>
+        </div>
+        <p>Type the information you wish to share secretely in the input field.</p>
+      </div>
+      <div class="step">
+        <div class="num">
+          <h2>2</h2>
+        </div>
+        <p>Click on Generate Link to generate a unique link for the data you wish to share.</p>
+      </div>
+      <div class="step">
+        <div class="num">
+          <h2>3</h2>
+        </div>
+        <p>Copy the generated link and send it to the intended personnel. The link is only available for one-time use.</p>
+      </div>
+    </div>
+    <div class="info">
+        <editor
+          v-model="secretData"
+          :init="{
+              height: '280',
+              menubar: false,
+              resize: false,
+              outputFormat: 'text',
+              plugins: 'lists image paste wordcount',
+              toolbar: 'undo redo  bold italic  forecolor backcolor  template codesample alignleft aligncenter alignright alignjustify | bullist numlist | link image',
+          }"
+        />
+        <button type="submit" @click="generateLink" :disabled="awaiting === '1'">Generate Link</button>
+        <dataLink v-model:dataURL="generateURL" v-model:disabled="disabled" v-model:visibility="visibility" @copyURL="copyURL(false)" />
+    </div>
+  </div>
+</template>
---
+++ src/components/nav.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export default {
+    name: "Nav",
+    methods: {
+      home() {
+        location.href = "/";
+      }
+    }
+  }
+</script>
+
+<template>
+  <nav>
+    <div class="logo">
+      <img src="./icons/logo.svg" alt="iMvula ICD Logo" width="150" @click="home" />
+    </div>
+  </nav>
+</template>
